855b28bc9571a8552dfaabef2954ca3dc36e3060
mihmin98 <mihmin98@yahoo.com>
2020-07-07 22:06:25 +0300

    Added opcode instructions in header

--- src/SM83.hpp
+++ src/SM83.hpp
@@ -18,28 +18,130 @@ class SM83
 
     /* OPCODE INSTRUCTIONS */
 
-    /* 8 BIT LOAD INSTRUCIONS */
+    /* 8 BIT ARITHMETIC INSTRUCTIONS */
+    void op_adc_a_r8(const uint8_t &r8);
+    void op_adc_a_addr_hl();
+    void op_adc_a_n8();
+    void op_add_a_r8(const uint8_t &r8);
+    void op_add_a_addr_hl();
+    void op_add_a_n8();
+    void op_dec_r8(uint8_t &r8);
+    void op_dec_addr_hl();
+    void op_inc_r8(uint8_t &r8);
+    void op_inc_addr_hl();
+    void op_sbc_a_r8(const uint8_t &r8);
+    void op_sbc_a_addr_hl();
+    void op_sbc_a_n8();
+    void op_sub_a_r8(const uint8_t &r8);
+    void op_sub_a_addr_hl();
+    void op_sub_a_n8();
 
-    void op_ld_r1_r2(uint8_t &r1, const uint8_t &r2);
-    void op_ld_r_n(uint8_t &r);
-    void op_ld_r_addr_r16(uint8_t &r1, const uint8_t &r2_high, const uint8_t &r2_low);
-    void op_ld_addr_r16_r(const uint8_t &r1_high, const uint8_t &r1_low, const uint8_t &r2);
-    void op_ld_addr_r16_n(const uint8_t &r_high, const uint8_t &r_low);
-    void op_ld_r_addr_nn(uint8_t &r);
-    void op_ld_addr_nn_r(const uint8_t &r);
-    void op_ldh_r_addr_r8(uint8_t &r1, const uint8_t &r2);
-    void op_ldh_addr_r8_r(const uint8_t &r1, const uint8_t &r2);
-    void op_ldh_r_addr_n(uint8_t &r);
-    void op_ldh_addr_n_r(const uint8_t &r);
-    void op_ld_r_addr_r16_dec(uint8_t &r1, uint8_t &r2_high, uint8_t &r2_low);
-    void op_ld_addr_r16_dec_r(uint8_t &r1_high, uint8_t &r1_low, const uint8_t &r2);
-    void op_ld_r_addr_r16_inc(uint8_t &r1, uint8_t &r2_high, uint8_t &r2_low);
-    void op_ld_addr_r16_inc_r(uint8_t &r1_high, uint8_t &r1_low, const uint8_t &r2);
+    /* 8 BIT LOGIC INSTRUCTIONS */
+    void op_and_a_r8(const uint8_t &r8);
+    void op_and_a_addr_hl();
+    void op_and_a_n8();
+    void op_cp_a_r8(const uint8_t &r8);
+    void op_cp_a_addr_hl();
+    void op_cp_a_n8();
+    void op_or_a_r8(const uint8_t &r8);
+    void op_or_a_addr_hl();
+    void op_or_a_n8();
+    void op_xor_a_r8(const uint8_t &r8);
+    void op_xor_a_addr_hl();
+    void op_xor_a_n8();
 
-    /* 16 BIT LOAD INSTRUCIONS */
-    void op_ld_rr_nn(uint8_t &r_high, uint8_t &r_low);
-    void op_ld_addr_nn_sp();
-    
+    /* 16 BIT ARITHMETIC INSTRUCTIONS */
+    void op_add_hl_r16(const uint8_t &r16_high, const uint8_t &r16_low);
+    void op_dec_r16(uint8_t &r16);
+    void op_inc_r16(uint8_t &r16);
+
+    /* BIT OPERATIONS INSTRUCTIONS */
+    void op_bit_u3_r8(uint8_t u3, const uint8_t &r8);
+    void op_bit_u3_addr_hl(uint8_t u3);
+    void op_res_u3_r8(uint8_t u3, uint8_t &r8);
+    void op_res_u3_addr_hl(uint8_t u3);
+    void op_set_u3_r8(uint8_t u3, uint8_t &r8);
+    void op_set_u3_addr_hl(uint8_t u3);
+    void op_swap_r8(uint8_t &r8);
+    void op_swap_addr_hl();
+
+    /* BIT SHIFT INSTRUCTIONS */
+    void op_rl_r8(uint8_t &r8);
+    void op_rl_addr_hl();
+    void op_rla();
+    void op_rlc_r8(uint8_t &r8);
+    void op_rlc_addr_hl();
+    void op_rlca();
+    void op_rr_r8(uint8_t &r8);
+    void op_rr_addr_hl();
+    void op_rra();
+    void op_rrc_r8(uint8_t &r8);
+    void op_rrc_addr_hl();
+    void op_rrca();
+    void op_sla_r8(uint8_t &r8);
+    void op_sla_addr_hl();
+    void op_sra_r8(uint8_t &r8);
+    void op_sra_addr_hl();
+    void op_srl_r8(uint8_t &r8);
+    void op_srl_addr_hl();
+
+    /* LOAD INSTRUCTIONS */
+    void op_ld_r8_r8(uint8_t &r8_1, const uint8_t &r8_2);
+    void op_ld_r8_n8(uint8_t &r8);
+    void op_ld_r16_n16(uint8_t &r16_high, uint8_t &r16_low);
+    void op_ld_addr_hl_r8(const uint8_t &r8);
+    void op_ld_addr_hl_n8();
+    void op_ld_r8_addr_hl(uint8_t &r8);
+    void op_ld_addr_r16_a(const uint8_t &r16_high, const uint8_t &r16_low);
+    void op_ld_addr_n16_a();
+    void op_ldh_addr_n16_a();
+    void op_ldh_addr_c_a();
+    void op_ld_a_addr_r16(const uint8_t &r16_high, const uint8_t &r16_low);
+    void op_ld_a_addr_n16();
+    void op_ldh_a_addr_n16();
+    void op_ldh_a_addr_c();
+    void op_ld_addr_hl_inc_a();
+    void op_ld_addr_hl_dec_a();
+    void op_ld_a_addr_hl_inc();
+    void op_ld_a_addr_hl_dec();
+
+    /* JUMP AND SUBROUTINE INSTRUCTIONS */
+    void op_call_n16();
+    void op_call_cc_n16(uint8_t flag_bit, uint8_t required_value);
+    void op_jp_hl();
+    void op_jp_n16();
+    void op_jp_cc_n16(uint8_t flag_bit, uint8_t required_value);
+    void op_jr_e8();
+    void op_jr_cc_e8(uint8_t flag_bit, uint8_t required_value);
+    void op_ret_cc(uint8_t flag_bit, uint8_t required_value);
+    void op_ret();
+    void op_reti();
+    void op_rst(uint16_t addr);
+
+    /* STACK OPERATIONS INSTRUCTIONS */
+    void op_add_hl_sp();
+    void op_add_sp_e8();
+    void op_dec_sp();
+    void op_inc_sp();
+    void op_ld_sp_n16();
+    void op_ld_addr_n16_sp();
+    void op_ld_hl_sp_plus_e8();
+    void op_ld_sp_hl();
+    void op_pop_af();
+    void op_pop_r16(uint8_t &r16_high, uint8_t &r16_low);
+    void op_push_af();
+    void op_push_r16(const uint8_t &r16_high, const uint8_t &r16_low);
+
+    /* MISC INSTRUCTIONS */
+    void op_ccf();
+    void op_cpl();
+    void op_daa();
+    void op_di();
+    void op_ei();
+    void op_halt();
+    void op_nop();
+    void op_scf();
+    void op_stop();
 
     /* OPCODES */
 };
