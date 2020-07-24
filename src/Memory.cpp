66bb2724abea82e9fe4a74f9aa2a4db36fd7f991
mihmin98 <mihmin98@yahoo.com>
2020-07-24 10:16:55 +0300

    Created Memory class

---
+++ src/Memory.cpp
@@ -0,0 +1,116 @@
+#include "Memory.hpp"
+
+Memory::Memory()
+{
+    currentVramBank = 0;
+    currentWramBank = 1;
+}
+
+uint8_t Memory::readmem(uint16_t addr)
+{
+    // ROM + External RAM
+    if (addr < 0x8000 || (addr >= 0xA000 && addr < 0xC000))
+        return rom->readmem(addr);
+
+    // VRAM
+    if (addr >= 0x8000 && addr < 0xA000) {
+        uint16_t actualAddr = (addr - 0x8000) + currentVramBank * 0x2000;
+        return vram[actualAddr];
+    }
+
+    // WRAM Bank 0
+    if (addr >= 0xC000 && addr < 0xD000)
+        return wram[addr - 0xC000];
+    
+    // Switchable WRAM Bank
+    if (addr >= 0xD000 && addr < 0xE000) {
+        uint16_t actualAddr = (addr - 0xD000) + currentWramBank * 0x1000;
+        return wram[actualAddr];
+    }
+
+    // ECHO RAM
+    if (addr >= 0xE000 && addr < 0xFE00) {
+        if (addr >= 0xE000 && addr < 0xF000)
+            return wram[addr - 0xE000];
+
+        if (addr >= 0xF000 && addr < 0xFE00) {
+            uint16_t actualAddr = (addr - 0xF000) + currentWramBank * 0x1000;
+            return wram[actualAddr];
+        }
+    }
+
+    // OAM
+    if (addr >= 0xFE00 && addr < 0xFEA0)
+        return oam[addr - 0xFE00];
+
+    // Unused range
+    if (addr >= 0xFEA0 && addr < 0xFF00)
+        return 0x00;
+
+    // I/O Registers
+    if (addr >= 0xFF00 && addr < 0xFF80)
+        return ioRegisters[addr - 0xFF00];
+
+    // HRAM
+    if (addr >= 0xFF80 && addr < 0xFFFF)
+        return hram[addr - 0xFF80];
+
+    // IE Register
+    if (addr == 0xFFFF)
+        return ieRegister;
+}
+
+void Memory::writemem(uint8_t val, uint16_t addr)
+{
+    // ROM + External RAM
+    if (addr < 0x8000 || (addr >= 0xA000 && addr < 0xC000))
+        rom->writemem(val, addr);
+
+    // VRAM
+    if (addr >= 0x8000 && addr < 0xA000) {
+        uint16_t actualAddr = (addr - 0x8000) + currentVramBank * 0x2000;
+        vram[actualAddr] = val;
+    }
+
+    // WRAM Bank 0
+    if (addr >= 0xC000 && addr < 0xD000)
+        wram[addr - 0xC000] = val;
+    
+    // Switchable WRAM Bank
+    if (addr >= 0xD000 && addr < 0xE000) {
+        uint16_t actualAddr = (addr - 0xD000) + currentWramBank * 0x1000;
+        wram[actualAddr] = val;
+    }
+
+    // ECHO RAM
+    if (addr >= 0xE000 && addr < 0xFE00) {
+        if (addr >= 0xE000 && addr < 0xF000)
+            wram[addr - 0xE000] = val;
+
+        if (addr >= 0xF000 && addr < 0xFE00) {
+            uint16_t actualAddr = (addr - 0xF000) + currentWramBank * 0x1000;
+            wram[actualAddr] = val;
+        }
+    }
+
+    // OAM
+    if (addr >= 0xFE00 && addr < 0xFEA0)
+        oam[addr - 0xFE00] = val;
+
+    // Unused range
+    if (addr >= 0xFEA0 && addr < 0xFF00)
+        return;
+
+    // I/O Registers
+    if (addr >= 0xFF00 && addr < 0xFF80)
+        ioRegisters[addr - 0xFF00] = val;
+
+    // HRAM
+    if (addr >= 0xFF80 && addr < 0xFFFF)
+        hram[addr - 0xFF80] = val;
+
+    // IE Register
+    if (addr == 0xFFFF)
+        ieRegister = val;
+}
+
---
+++ src/Memory.hpp
@@ -0,0 +1,48 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#pragma once
+#include <cstdint>
+#include <cstring>
+#include "ROM.hpp"
+#include "GameBoy.hpp"
+
+#define MEM_ROM0_START 0x0000
+#define MEM_ROMX_START 0x4000
+#define MEM_VRAM_START 0x8000
+#define MEM_EXT_RAM_START 0xA000
+#define MEM_WRAM0_START 0xC000
+#define MEM_WRAMX_START 0xD000
+#define MEM_ECHO_START 0xE000
+#define MEM_OAM_START 0xFE00
+#define MEM_UNUSED_START 0xFEA0
+#define MEM_IO_START 0xFF00
+#define MEM_HRAM_START 0xFF80
+
+class GameBoy;
+
+class Memory
+{
+  public:
+
+    ROM *rom; // Pointer to the ROM
+
+    /* MEMORY AREAS */
+
+    uint8_t vram[2 * 0x2000];
+    uint8_t currentVramBank;
+    uint8_t wram[8 * 0x1000];
+    uint8_t currentWramBank;
+    uint8_t oam[0xA0];
+    uint8_t ioRegisters[0x80];
+    uint8_t hram[0x7F];
+    uint8_t ieRegister;
+
+    Memory(); 
+    
+    uint8_t readmem(uint16_t addr);
+    void writemem(uint8_t val, uint16_t addr);
+};
+ 
+#endif // __MEMORY_H__
+
