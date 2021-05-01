7fe87edea47c3bd07397dcda2c615cb2bfd0b327
mihmin98 <mihmin98@yahoo.com>
2021-05-02 00:46:58 +0300

    Added DMA transfer functionality

--- src/Memory/Memory.cpp
+++ src/Memory/Memory.cpp
@@ -74,8 +74,8 @@ uint8_t Memory::readmem(uint16_t addr, bool bypass, bool bypassOamDma)
         if (ppu->oamDmaActive && !bypass && !bypassOamDma)
             return 0xFF;
         else {
-            LcdMode mode = ppu->getLcdMode();
-            if ((mode == OAM_SEARCH && !bypass) || (mode == DRAW && !bypass))
+            LcdMode lcdMode = ppu->getLcdMode();
+            if ((lcdMode == OAM_SEARCH && !bypass) || (lcdMode == DRAW && !bypass))
                 return 0xFF;
             else
                 return oam[addr - MEM_OAM_START];
@@ -95,26 +95,30 @@ uint8_t Memory::readmem(uint16_t addr, bool bypass, bool bypassOamDma)
             return 0xFF;
         } else {
             // TODO: Case for 0xFF55 (vram hblank dma remaining bytes - 1)
+            // This should be done by the ppu? Already done?
+            // there is no need for a special case or something
 
-            LcdMode mode = ppu->getLcdMode();
+            LcdMode lcdMode = ppu->getLcdMode();
 
-            if (addr == 0xFF69) {
-                // BGPD (Backgroud Color Palette Data)
-                if (mode == DRAW && !bypass) {
-                    return 0xFF;
-                } else {
-                    uint8_t index = ppu->getBgColorPaletteIndex() & 0x3F;
-                    return cgbBgColorPalette[index];
+            if (mode == CGB) {
+                if (addr == 0xFF69) {
+                    // BGPD (Backgroud Color Palette Data)
+                    if (lcdMode == DRAW && !bypass) {
+                        return 0xFF;
+                    } else {
+                        uint8_t index = ppu->getBgColorPaletteIndex() & 0x3F;
+                        return cgbBgColorPalette[index];
+                    }
                 }
-            }
 
-            if (addr == 0xFF6B) {
-                // OBPD (Object Color Palette Data)
-                if (mode == DRAW && !bypass) {
-                    return 0xFF;
-                } else {
-                    uint8_t index = ppu->getObjColorPaletteIndex() & 0x3F;
-                    return cgbObjColorPalette[index];
+                if (addr == 0xFF6B) {
+                    // OBPD (Object Color Palette Data)
+                    if (lcdMode == DRAW && !bypass) {
+                        return 0xFF;
+                    } else {
+                        uint8_t index = ppu->getObjColorPaletteIndex() & 0x3F;
+                        return cgbObjColorPalette[index];
+                    }
                 }
             }
 
@@ -189,9 +193,9 @@ void Memory::writemem(uint8_t val, uint16_t addr, bool bypass, bool bypassOamDma
     // OAM
     if (addr >= MEM_OAM_START && addr < MEM_UNUSED_START) {
         if (!ppu->oamDmaActive || (bypass && bypassOamDma)) {
-            LcdMode mode = ppu->getLcdMode();
-            if ((mode != OAM_SEARCH || (bypass && bypassOamDma)) &&
-                (mode != DRAW || (bypass && bypassOamDma)))
+            LcdMode lcdMode = ppu->getLcdMode();
+            if ((lcdMode != OAM_SEARCH || (bypass && bypassOamDma)) &&
+                (lcdMode != DRAW || (bypass && bypassOamDma)))
                 oam[addr - MEM_OAM_START] = val;
         }
     }
@@ -203,28 +207,60 @@ void Memory::writemem(uint8_t val, uint16_t addr, bool bypass, bool bypassOamDma
     // I/O Registers
     if (addr >= MEM_IO_START && addr < MEM_HRAM_START) {
         if (!ppu->oamDmaActive || bypass) {
-            LcdMode mode = ppu->getLcdMode();
-            // TODO: Add special case for DMA Transfer (0xFF46) and others for CGB
+            LcdMode lcdMode = ppu->getLcdMode();
 
-            if (addr == 0xFF69) {
-                // BGPD (Backgroud Color Palette Data)
-                if (mode != DRAW || bypass) {
-                    uint8_t index = ppu->getBgColorPaletteIndex();
-                    cgbBgColorPalette[index & 0x3F] = val;
-                    // AutoIncrement
-                    if (index & 0x80)
-                        ppu->setBgColorPaletteIndex(0x80 | (((index & 0x3F) + 1) & 0x3F));
-                }
+            if (addr == 0xFF46) {
+                // Trigger OAM DMA
+                ppu->oamDmaActive = true;
+                ppu->oamDmaCurrentCycles = 0;
+                ioRegisters[addr - MEM_IO_START] = val;
             }
 
-            if (addr == 0xFF6B) {
-                // OBPD (Object Color Palette Data)
-                if (mode != DRAW || bypass) {
-                    uint8_t index = ppu->getObjColorPaletteIndex();
-                    cgbObjColorPalette[index & 0x3F] = val;
-                    // AutoIncrement
-                    if (index & 0x80)
-                        ppu->setObjColorPaletteIndex(0x80 | (((index & 0x3F) + 1) & 0x3F));
+            if (mode == CGB) {
+                if (addr == 0xFF55) {
+                    // VRAM DMA
+                    if (ppu->vramHblankDmaActive) {
+                        if ((val & 0x80) == 0) {
+                            // Stop dma if bit 7 is 0, afterwards set bit 7 to 1
+                            ppu->vramHblankDmaActive = false;
+                            ioRegisters[addr - MEM_IO_START] =
+                                (1 << 7) || (ioRegisters[addr - MEM_IO_START] & 0x7F);
+                        }
+                    } else if (!ppu->vramHblankDmaActive && !ppu->vramGeneralDmaActive) {
+                        // Start dma
+                        ppu->vramDmaLength = val & 0x7F;
+                        ppu->vramDmaTransferredBytes = 0;
+                        ppu->vramDmaCurrentCycles = 0;
+                        if (val & 80)
+                            ppu->vramHblankDmaActive = true;
+                        else
+                            ppu->vramGeneralDmaActive = true;
+
+                        // Set bit 7 to 0
+                        ioRegisters[addr - MEM_IO_START] = val & 0x7F;
+                    }
+                }
+
+                if (addr == 0xFF69) {
+                    // BGPD (Backgroud Color Palette Data)
+                    if (lcdMode != DRAW || bypass) {
+                        uint8_t index = ppu->getBgColorPaletteIndex();
+                        cgbBgColorPalette[index & 0x3F] = val;
+                        // AutoIncrement
+                        if (index & 0x80)
+                            ppu->setBgColorPaletteIndex(0x80 | (((index & 0x3F) + 1) & 0x3F));
+                    }
+                }
+
+                if (addr == 0xFF6B) {
+                    // OBPD (Object Color Palette Data)
+                    if (lcdMode != DRAW || bypass) {
+                        uint8_t index = ppu->getObjColorPaletteIndex();
+                        cgbObjColorPalette[index & 0x3F] = val;
+                        // AutoIncrement
+                        if (index & 0x80)
+                            ppu->setObjColorPaletteIndex(0x80 | (((index & 0x3F) + 1) & 0x3F));
+                    }
                 }
             }
 
